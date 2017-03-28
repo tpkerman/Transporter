5d930631b36ed0e5681855da0fdb7eb4aeec9bf5
tpkerman <erduley@live.com>
2017-03-28 13:40:00 -0500

    Summary
    Description

---
+++ MechjebWrapper.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using System.Reflection;
+using MuMech;
+
+namespace Transporter
+{
+    class MechjebWrapper
+    {
+        
+        System.Type CoreType;
+
+        public bool Initialized = false;
+        public static Vessel vessel { get { return FlightGlobals.ActiveVessel; } }
+        PartModule core = null;
+        
+
+        bool GetCore()
+        {
+            foreach (Part p in vessel.parts)
+            {
+                foreach (PartModule module in p.Modules)
+                {
+                    if (module.GetType() == CoreType)
+                    {
+                        core = module;
+                        return true;
+                    }
+                }
+
+            }
+            return false;
+        }
+
+        System.Type FindMechJebModule(string module)
+        {
+            Type type = null;
+            AssemblyLoader.loadedAssemblies.TypeOperation(t =>
+            {
+                if (t.FullName == module)
+                {
+                    type = t;
+                }
+            });
+
+            return type;
+        }
+
+        public bool init()
+        {
+            if (Initialized)
+                return true;
+            CoreType = FindMechJebModule("MuMech.MechJebCore");
+
+            if (CoreType == null)
+            {
+              
+                return false;
+            }
+            if (!GetCore())
+            {
+             
+                return false;
+            }
+
+            Initialized = true;
+            return true;
+        }
+
+        public EditableAngle mjLat;
+        public void getCoOrd()
+        {
+            // get target field info in mechjeb core calss
+            var targetInfo = CoreType.GetField("target");
+            // get object instance of "target" field in mechjeb core
+            var targetControllerObject = targetInfo.GetValue(core);
+
+            // get the class type for target controller
+            System.Type MechJebModuleTargetControllerType = FindMechJebModule("MuMech.MechJebModuleTargetController");
+
+            // get targetLatitude field info in target controller class
+            var targetLatitudeInfo = MechJebModuleTargetControllerType.GetField("targetLatitude");
+            // get object (value) of targetLatitude in target field
+            var targetLatitudeObject = targetLatitudeInfo.GetValue(targetControllerObject);
+    // type cast to actual editable angle 
+            mjLat = (EditableAngle)targetLatitudeObject;
+
+        }
+    }
+}
---
+++ Transporter.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections.Generic;
+using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using KSP;
+using KSP.IO;
+using KSP.UI.Screens.Flight;
+using MuMech;
+
+namespace Transporter
+{
+    
+    public class transportPlugin : PartModule
+    {
+        MechjebWrapper mucore = new MechjebWrapper();
+        ProtoCrewMember buffer;
+        Part destination;
+        Vessel targetVes;
+        public bool refresh = false;
+        
+        public static Vessel thisVes { get { return FlightGlobals.ActiveVessel; } }
+        
+
+       
+
+        [KSPField(isPersistant = false, guiActive = true, guiName = "Active Vessel")]
+        public string vesselName;
+
+        [KSPField(isPersistant = false, guiActive = true, guiName = "Occupant")]
+        public string inPod;
+
+        [KSPField(isPersistant = false, guiActive = true, guiName = "Buffer")]
+        public string inBuffer;
+
+        [KSPField(isPersistant = false, guiActive = true, guiName = "Target Vessel")]
+        public string targetVesName;
+
+        [KSPField(isPersistant = false, guiActive = true, guiName = "CoOrd")]
+        public double displayLat;
+
+        [KSPField(isPersistant = false, guiActive = true, guiName = "Target Status")]
+        public string targetStat = "Error";
+        
+        [KSPField(isPersistant = false, guiActive = true, guiName = "Distance to Target", guiFormat = "F3")]
+        public double distance;
+
+        [KSPField(isPersistant = false, guiActive = true, guiName = "MJ Core")]
+        public bool MJActive;
+
+        [KSPEvent(isPersistent = false, guiActive = true, guiName = "Init")]
+        public void tryInit()
+        {
+            mucore.init();
+            
+        }
+
+        
+
+        [KSPEvent(isPersistent = false, guiActive = true, guiName = "CoOrd Pick")]
+        public void tryPick()
+        {
+            mucore.getCoOrd();
+            displayLat = (double)mucore.mjLat;
+        }
+
+        [KSPEvent(active = true, guiActive = true, guiName = "Send Crewmember", name = "transfer")]
+        public void sendEvent()
+        {
+            if (targetVes.loaded == false)
+            {
+                targetVes.Load();
+            }
+            if ((targetVes.parts.Find(x => x.name.Contains("landerCabinSmall"))) & (targetStat.Equals("Ready")))
+            {
+                buffer = this.part.protoModuleCrew[0];
+                this.part.RemoveCrewmember(this.part.protoModuleCrew[0]);
+                destination = this.vessel.targetObject.GetVessel().parts.Find(destination => destination.name.Contains("landerCabinSmall"));
+                //this.vessel.targetObject.GetVessel().MakeActive();
+                destination.force_activate();
+                destination.AddCrewmember(buffer);
+            }
+
+            if (distance >= 2200 & (targetVes.isActiveVessel == false))
+            {
+                targetVes.Unload();
+            }
+        }
+
+        [KSPEvent(active = true, guiActive = true, guiName = "Recieve Crewmember", guiActiveUncommand = true,requireFullControl = false, name = "recieve")]
+        public void recieveEvent()
+        {
+            buffer = targetVes.GetVesselCrew()[0];
+            targetVes.Die();
+            targetVes.DestroyVesselComponents();
+            targetVes.Unload();
+            this.part.AddCrewmember(buffer);
+            KerbalPortraitGallery.Instance.Awake();
+            KerbalPortraitGallery.Instance.Start();
+            refresh = true;
+        }
+
+        [KSPEvent(active = true, guiActive = true, guiName = "Hail Destination", name = "hail")]
+        public void hailDestination()
+        {
+            
+
+            if (targetVes.loaded == false)
+            {
+                targetVes.Load();
+            }
+            if ((targetVes.GetCrewCapacity() > targetVes.GetCrewCount()) & (distance <= 1350000))
+            {
+                targetStat = "Ready";
+            }
+            else
+            {
+                targetStat = "Error";
+            }
+            if (distance >= 2200 & (targetVes.isActiveVessel == false))
+            {
+                targetVes.Unload();
+            }
+            
+        }
+
+        
+        
+
+        public override void OnUpdate()
+        {
+            inPod = this.part.protoModuleCrew[0].name.ToString();
+            
+
+            //mucore.getCoOrd();
+            //displayLat = mucore.mjLat;
+
+
+
+        }
+        public void FixedUpdate()
+        {
+            distance = Vector3.Distance(FlightGlobals.ActiveVessel.GetWorldPos3D(), this.vessel.targetObject.GetVessel().GetWorldPos3D());
+            targetVes = this.vessel.targetObject.GetVessel();
+            targetVesName = targetVes.protoVessel.vesselName;
+            if(mucore.Initialized)
+            { MJActive = true; }
+            
+            if (refresh)
+            {
+                this.part.protoModuleCrew[0].KerbalRef.state = Kerbal.States.ALIVE;
+                this.part.protoModuleCrew[0].KerbalRef.SetVisibleInPortrait(true);
+                
+                this.part.protoModuleCrew[0].KerbalRef.randomizeOnStartup = false;
+                this.part.protoModuleCrew[0].KerbalRef.Start();
+                KerbalPortraitGallery.Instance.SpawnPortrait(buffer.KerbalRef);
+                KerbalPortraitGallery.Instance.StartReset(thisVes);
+
+                refresh = false;
+            }
+        }
+    }
+
+
+    
+    
+}
+
